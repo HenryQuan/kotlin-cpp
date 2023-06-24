60837c8e877f43f43ef318bb7e8b98b7875b41b2
Yiheng <development.henryquan@gmail.com>
2023-06-24 19:41:23 +1000

    added kotlin native project

---
+++ kotlin/src/nativeMain/kotlin/Main.kt
@@ -0,0 +1,6 @@
+import lib.math_op
+
+fun main() {
+    val result = math_op(5, 6)
+    println("Hello, Kotlin/Native!, $result")
+}
---
+++ kotlin/src/nativeMain/kotlin/lib/math_op.kt
@@ -0,0 +1,6 @@
+package lib
+
+@CName("math_op")
+fun math_op(a: Int, b: Int): Int {
+    return a + b
+}
